56c67904bdcd5f3b9da9491080db2d1cdfe9ce95
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-27 12:23:38 -0400

    finished number and table utilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,45 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                builder.append(i * j);
+                if (j < 5) {
+                    builder.append(" | ");
+                }
+            }
+            builder.append(" \n");
+        }
+        return builder.toString();
     }
 
+
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                builder.append(i * j);
+                if (j < 10) {
+                    builder.append(" | ");
+                }
+            }
+            builder.append(" \n");
+        }
+        return builder.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i <= tableSize i++) {
+            for (int j = 1; j <= 5; j++) {
+                builder.append(tableSize * j);
+                if (j < tableSize) {
+                    builder.append(" | ");
+                }
+            }
+            builder.append(" \n");
+        }
+        return builder.toString();
     }
 }
